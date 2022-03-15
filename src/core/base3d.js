d0bba1419eccabb3d1efe2685138ad2eec883279
Stephen-wzw <wzw15292257101@163.com>
2022-03-15 14:52:29 +0800

    update

--- src/core/base3d.js
+++ src/core/base3d.js
@@ -1,4 +1,7 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
 import throttle from "@/utils/throttle";
 
 class Base3d {
@@ -7,6 +10,8 @@ class Base3d {
     this.scene;
     this.camera;
     this.renderer;
+    this.model;
+
     // TODO 状态监控
     // this.stats = document.getElementById(statsId);
     this.init();
@@ -17,6 +22,10 @@ class Base3d {
     this.initScene();
     this.initCamera();
     this.initRenderer();
+    this.initControls();
+
+    // 添加物体
+    this.addMesh();
 
     // 监听场景大小改变，调整渲染尺寸
     window.addEventListener("resize", throttle(this.onWindowResize.bind(this), 50, {
@@ -46,7 +55,28 @@ class Base3d {
     this.renderer.setPixelRatio(window.devicePixelRatio);
     // 渲染尺寸大小
     this.renderer.setSize(window.innerWidth, window.innerHeight);
+
     this.container.appendChild(this.renderer.domElement);
+  }
+
+  initControls() {
+    const controls = new OrbitControls(this.camera, this.renderer.domElement);
+    controls.target.set( 0, 0.5, 0 );
+    controls.update();
+    controls.enablePan = false;
+    controls.enableDamping = true;
+  }
+
+  setModel(modelName) {
+    const loader = new GLTFLoader().setPath("files/gltf/");
+    loader.load(modelName, gltf => {
+      this.model = gltf.scene.children[0];
+      this.scene.add(this.model);
+    })
+  }
+  
+  addMesh() {
+    this.setModel("bag2.glb");
   }
 
   animate() {
