cd325148871816e33daf996dc07d783fad23e81d
dribble-njr <wzw15292257101@163.com>
2023-04-18 18:14:26 +0800

    chore: lod

--- src/core/viewer.js
+++ src/core/viewer.js
@@ -5,6 +5,7 @@ import {
   BufferGeometry,
   DirectionalLight,
   GridHelper,
+  Group,
   LinearEncoding,
   LoaderUtils,
   LoadingManager,
@@ -24,6 +25,8 @@ import Stats from 'three/examples/jsm/libs/stats.module.js'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+
+import { BufferGeometryUtils } from 'three/examples/jsm/utils/BufferGeometryUtils.js'
 
 // 提供参数设置
 import { GUI } from 'dat.gui'
@@ -59,7 +62,7 @@ class Viewer {
     this.content = null
     // 面板
     this.gui = null
-    
+
     this.state = {
       playbackSpeed: 1.0,
       actionStates: {},
@@ -69,27 +72,35 @@ class Viewer {
       // Lights
       addLights: true,
       ambientIntensity: 1.0,
-      ambientColor: 0xFFFFFF,
+      ambientColor: 0xffffff,
       directIntensity: 0.8 * Math.PI, // TODO(#116)
-      directColor: 0xFFFFFF,
+      directColor: 0xffffff,
     }
 
     // 性能监控
     this.stats = new Stats()
     this.stats.dom.height = '48px'
-    Array.prototype.forEach.call(this.stats.dom.children, (child) => (child.style.display = ''))
+    Array.prototype.forEach.call(
+      this.stats.dom.children,
+      (child) => (child.style.display = '')
+    )
 
     this.scene = new Scene()
-    
-    this.camera = new PerspectiveCamera(60, el.clientWidth / el.clientHeight, 0.01, 1000)
+
+    this.camera = new PerspectiveCamera(
+      60,
+      el.clientWidth / el.clientHeight,
+      0.01,
+      1000
+    )
     this.scene.add(this.camera)
 
-    this.renderer = new WebGLRenderer({antialias: true})         // 抗锯齿
-    this.renderer.physicallyCorrectLights = true                 // 使用正确的照明模式
-    this.renderer.outputEncoding = sRGBEncoding                  // 输出 sRGB
-    this.renderer.setClearColor(0xcccccc)                        // 设置颜色
-    this.renderer.setPixelRatio(window.devicePixelRatio)         // 设置屏幕像素比
-    this.renderer.setSize(el.clientWidth, el.clientHeight)       // 调整 canvas 大小
+    this.renderer = new WebGLRenderer({ antialias: true }) // 抗锯齿
+    this.renderer.physicallyCorrectLights = true // 使用正确的照明模式
+    this.renderer.outputEncoding = sRGBEncoding // 输出 sRGB
+    this.renderer.setClearColor(0xffffff) // 设置颜色
+    this.renderer.setPixelRatio(window.devicePixelRatio) // 设置屏幕像素比
+    this.renderer.setSize(el.clientWidth, el.clientHeight) // 调整 canvas 大小
 
     // 轨道控制器
     this.controls = new OrbitControls(this.camera, this.renderer.domElement)
@@ -140,15 +151,15 @@ class Viewer {
     )
 
     // 点击选中
-    window.addEventListener("click", this.onMouseClick.bind(this))
+    window.addEventListener('click', this.onMouseClick.bind(this))
   }
 
   animate() {
     requestAnimationFrame(this.animate)
-    
+
     this.controls.update()
     this.stats.update()
-    
+
     if (this.renderEnable) {
       this.render()
     }
@@ -161,7 +172,7 @@ class Viewer {
       clearTimeout(this.timeOut)
     }
     this.timeOut = setTimeout(() => {
-      this.renderEnable = false
+      // this.renderEnable = false
     }, time)
   }
 
@@ -177,15 +188,21 @@ class Viewer {
   }
 
   onWindowResize() {
-    const {clientHeight, clientWidth} = this.el.parentElement
+    const { clientHeight, clientWidth } = this.el.parentElement
 
     this.camera.aspect = clientWidth / clientHeight
     this.camera.updateProjectionMatrix()
     this.renderer.setSize(clientWidth, clientHeight)
 
-    this.axesCamera.aspect = this.axesDiv.clientWidth / this.axesDiv.clientHeight
+    this.axesCamera.aspect =
+      this.axesDiv.clientWidth / this.axesDiv.clientHeight
     this.axesCamera.updateProjectionMatrix()
-    this.axesRenderer.setSize(this.axesDiv.clientWidth, this.axesDiv.clientHeight)
+    this.axesRenderer.setSize(
+      this.axesDiv.clientWidth,
+      this.axesDiv.clientHeight
+    )
+
+    this.timeRender(1000)
   }
 
   /**
@@ -196,8 +213,17 @@ class Viewer {
     // 点击偏移问题
     // https://blog.csdn.net/u013090676/article/details/77188088
     const canvas = document.querySelector('.viewer')
-    this.mouse.x = ((e.clientX - canvas.getBoundingClientRect().left) / canvas.offsetWidth) * 2 - 1
-    this.mouse.y = - ((e.clientY - canvas.getBoundingClientRect().top) / canvas.offsetHeight) * 2 + 1
+    this.mouse.x =
+      ((e.clientX - canvas.getBoundingClientRect().left) / canvas.offsetWidth) *
+        2 -
+      1
+    this.mouse.y =
+      -(
+        (e.clientY - canvas.getBoundingClientRect().top) /
+        canvas.offsetHeight
+      ) *
+        2 +
+      1
 
     // 通过鼠标点的位置和当前相机的矩阵计算出raycaster
     this.raycaster.setFromCamera(this.mouse, this.camera)
@@ -218,7 +244,7 @@ class Viewer {
         this.originMaterial = this.intersected.material
         const material = new MeshLambertMaterial({
           color: 0xff0000,
-          opacity: 0.8
+          opacity: 0.8,
         })
         this.intersected.material = material
 
@@ -249,9 +275,11 @@ class Viewer {
         // URIs in a glTF file may be escaped, or not. Assume that assetMap is
         // from an un-escaped source, and decode all URIs before lookups.
         // See: https://github.com/donmccurdy/three-gltf-viewer/issues/146
-        const normalizedURL = rootPath + decodeURI(url)
-          .replace(baseURL, '')
-          .replace(/^(\.?\/)/, '')
+        const normalizedURL =
+          rootPath +
+          decodeURI(url)
+            .replace(baseURL, '')
+            .replace(/^(\.?\/)/, '')
 
         if (assetMap.has(normalizedURL)) {
           const blob = assetMap.get(normalizedURL)
@@ -275,42 +303,48 @@ class Viewer {
 
       const blobURLs = []
 
-      loader.load(url, gltf => {
-        const scene = gltf.scene || gltf.scenes[0]
+      loader.load(
+        url,
+        (gltf) => {
+          const scene = gltf.scene || gltf.scenes[0]
 
-        if (!scene) {
-          // Valid, but not supported by this viewer.
-          throw new Error(
-            'This model contains no scene, and cannot be viewed here. However,'
-            + ' it may contain individual 3D resources.'
-          )
-        }
-
-        this.setContent(scene)
-
-        blobURLs.forEach(URL.revokeObjectURL)
-
-        // See: https://github.com/google/draco/issues/349
-        // DRACOLoader.releaseDecoderModule();
-
-        resolve(gltf)
-
-        // 解析属性
-        let file
-        if (assetMap.size === 3) {
-          for (const [key, value] of assetMap) {
-            if (key.indexOf('json') != -1) file = value
+          if (!scene) {
+            // Valid, but not supported by this viewer.
+            throw new Error(
+              'This model contains no scene, and cannot be viewed here. However,' +
+                ' it may contain individual 3D resources.'
+            )
           }
 
-          const reader = new FileReader()
-          reader.addEventListener('loadend', e => {
-            this.json = JSON.parse(e.target.result)
-          })
-          reader.readAsText(file)
-        }
-      }, xhr => {
-        console.log((xhr.loaded / xhr.total) * 100 + "% loaded")
-      }, reject)
+          this.setContent(scene)
+
+          blobURLs.forEach(URL.revokeObjectURL)
+
+          // See: https://github.com/google/draco/issues/349
+          // DRACOLoader.releaseDecoderModule();
+
+          resolve(gltf)
+
+          // 解析属性
+          let file
+          if (assetMap.size === 2) {
+            for (const [key, value] of assetMap) {
+              if (key.indexOf('json') != -1) file = value
+            }
+
+            const reader = new FileReader()
+            reader.addEventListener('loadend', (e) => {
+              this.json = JSON.parse(e.target.result)
+            })
+            reader.readAsText(file)
+            console.log(this.json)
+          }
+        },
+        (xhr) => {
+          console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+        },
+        reject
+      )
     })
   }
 
@@ -335,16 +369,18 @@ class Viewer {
           details[1] = gltf.scene || gltf.scenes[0]
           
           loader.load('models/lod1.glb', (gltf) => {
-            details[2] = gltf.scene || gltf.scenes[0]
-            
+            details[2]= gltf.scene || gltf.scenes[0]
+
             this.lod = new LOD()
-            
-            for ( let i = 0; i < details.length; i ++ ) {
-              const mesh = details[i]
-              mesh.updateMatrix()
-              mesh.matrixAutoUpdate = false
-              this.lod.addLevel(mesh, i * 10000)
-					  }
+
+            for (let i = 0; i < details.length; i++) {
+              details[i].updateMatrix()
+              details[i].matrixAutoUpdate = false
+            }
+
+            this.lod.addLevel(details[2], 200000)
+            this.lod.addLevel(details[1], 100000)
+            this.lod.addLevel(details[0], 50000)
 
             this.lod.updateMatrix()
             this.lod.matrixAutoUpdate = true
@@ -363,7 +399,7 @@ class Viewer {
    */
   setContent(object) {
     this.clear()
-    
+
     // AABB 包围盒
     const box = new Box3().setFromObject(object)
     const size = box.getSize(new Vector3()).length()
@@ -371,20 +407,24 @@ class Viewer {
 
     this.controls.reset()
 
-    object.position.x += (object.position.x - center.x)
-    object.position.y += (object.position.y - center.y)
-    object.position.z += (object.position.z - center.z)
+    object.position.x += object.position.x - center.x
+    object.position.y += object.position.y - center.y
+    object.position.z += object.position.z - center.z
     this.controls.maxDistance = size * 10 // 最长拉远距离
     this.camera.near = size / 100
     this.camera.far = size * 100
     this.camera.updateProjectionMatrix()
+    console.log('包围盒大小', size)
 
     // 根据包围盒设置相机位置
     this.camera.position.copy(center)
-    this.camera.position.x += size / 2.0
-    this.camera.position.y += size / 5.0
-    this.camera.position.z += size / 2.0
+    this.camera.position.x += size * 1.0
+    this.camera.position.y += size * 2.0
+    this.camera.position.z += size * 1.0
     this.camera.lookAt(center)
+
+    const distance = this.camera.position.distanceTo(this.lod.position)
+    console.log('相机到物体的距离', distance)
 
     this.axesCamera.position.copy(this.camera.position)
     this.axesCamera.lookAt(this.axesScene.position)
@@ -401,7 +441,7 @@ class Viewer {
     this.state.addLights = true
 
     // Executes the callback on this object and all descendants.
-    this.content.traverse(node => {
+    this.content.traverse((node) => {
       if (node.isLight) {
         this.state.addLights = false
       } else if (node.isMesh) {
@@ -409,7 +449,7 @@ class Viewer {
         node.material.depthWrite = !node.material.transparent
         // node.material.alphaTest = 0.5
       }
-    });
+    })
 
     this.updateLights()
     this.updateTextureEncoding()
@@ -446,12 +486,15 @@ class Viewer {
     const state = this.state
 
     // 环境光
-    const light1  = new AmbientLight(state.ambientColor, state.ambientIntensity)
+    const light1 = new AmbientLight(state.ambientColor, state.ambientIntensity)
     light1.name = 'ambient_light'
     this.camera.add(light1)
 
     // 平行光
-    const light2  = new DirectionalLight(state.directColor, state.directIntensity)
+    const light2 = new DirectionalLight(
+      state.directColor,
+      state.directIntensity
+    )
     light2.position.set(0.5, 0, 0.866) // ~60º
     light2.name = 'main_light'
     this.camera.add(light2)
@@ -459,7 +502,7 @@ class Viewer {
     this.lights.push(light1, light2)
   }
 
-  updateTextureEncoding () {
+  updateTextureEncoding() {
     // 输入 sRGB
     const encoding = sRGBEncoding
     traverseMaterials(this.content, (material) => {
@@ -473,7 +516,7 @@ class Viewer {
   /**
    * 添加辅助坐标系
    */
-  addAxesHelper () {
+  addAxesHelper() {
     this.axesDiv = document.createElement('div')
     this.el.appendChild(this.axesDiv)
     this.axesDiv.classList.add('axes')
@@ -481,12 +524,20 @@ class Viewer {
     const { clientWidth, clientHeight } = this.axesDiv
 
     this.axesScene = new Scene()
-    this.axesCamera = new PerspectiveCamera(50, clientWidth / clientHeight, 0.1, 10)
+    this.axesCamera = new PerspectiveCamera(
+      50,
+      clientWidth / clientHeight,
+      0.1,
+      10
+    )
     this.axesScene.add(this.axesCamera)
 
     this.axesRenderer = new WebGLRenderer({ alpha: true })
     this.axesRenderer.setPixelRatio(window.devicePixelRatio)
-    this.axesRenderer.setSize(this.axesDiv.clientWidth, this.axesDiv.clientHeight)
+    this.axesRenderer.setSize(
+      this.axesDiv.clientWidth,
+      this.axesDiv.clientHeight
+    )
 
     this.axesCamera.up = this.camera.up
 
@@ -498,10 +549,10 @@ class Viewer {
   /**
    * 更新渲染
    */
-  updateDisplay () {
+  updateDisplay() {
     traverseMaterials(this.content, (material) => {
-      material.wireframe = this.state.wireframe;
-    });
+      material.wireframe = this.state.wireframe
+    })
 
     if (this.state.grid !== Boolean(this.gridHelper)) {
       if (this.state.grid) {
@@ -524,8 +575,12 @@ class Viewer {
   /**
    * 添加监控面板
    */
-  addGUI () {
-    const gui = this.gui = new GUI({autoPlace: false, width: 260, hideable: true})
+  addGUI() {
+    const gui = (this.gui = new GUI({
+      autoPlace: false,
+      width: 260,
+      hideable: true,
+    }))
 
     // Display controls.
     const dispFolder = gui.addFolder('Display')
@@ -537,19 +592,23 @@ class Viewer {
 
     // Lighting controls.
     const lightFolder = gui.addFolder('Lighting')
-    lightFolder.add(this.renderer, 'outputEncoding', {sRGB: sRGBEncoding, Linear: LinearEncoding})
+    lightFolder
+      .add(this.renderer, 'outputEncoding', {
+        sRGB: sRGBEncoding,
+        Linear: LinearEncoding,
+      })
       .onChange(() => {
         this.renderer.outputEncoding = Number(this.renderer.outputEncoding)
         traverseMaterials(this.content, (material) => {
           material.needsUpdate = true
         })
-      });
-    [
+      })
+    ;[
       lightFolder.add(this.state, 'addLights').listen(),
       lightFolder.add(this.state, 'ambientIntensity', 0, 2),
       lightFolder.addColor(this.state, 'ambientColor'),
       lightFolder.add(this.state, 'directIntensity', 0, 4), // TODO(#116)
-      lightFolder.addColor(this.state, 'directColor')
+      lightFolder.addColor(this.state, 'directColor'),
     ].forEach((ctrl) => ctrl.onChange(() => this.updateLights()))
 
     // Stats.
@@ -567,21 +626,21 @@ class Viewer {
     gui.open()
   }
 
-  clear () {
+  clear() {
     if (!this.content) return
 
     this.scene.remove(this.content)
 
     // dispose geometry
     this.content.traverse((node) => {
-      if ( !node.isMesh ) return
+      if (!node.isMesh) return
       node.geometry.dispose()
     })
 
     // dispose textures
     traverseMaterials(this.content, (material) => {
       MAP_NAMES.forEach((map) => {
-        if (material[ map ]) material[ map ].dispose()
+        if (material[map]) material[map].dispose()
       })
     })
   }
---
+++ src/service/index.js
@@ -0,0 +1,11 @@
+export async function getElement(url = '', data = {}) {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+
+  return response.json()
+}
